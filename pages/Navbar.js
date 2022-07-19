bad6a24185ed1f3a5c97aa3c82e8b733effd2667
Prateek Kashyap <94394757+bit-by-bits@users.noreply.github.com>
2022-07-19 17:28:22 +0530

    added navbar

---
+++ pages/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.nav_left}>
+        <Link href="/">
+          <div>Home</div>
+        </Link>
+      </div>
+      <div className={styles.nav_right}>
+        <Link href="/Upload">
+          <div>Add a file</div>
+        </Link>
+        <Link href="/Login">
+          <div className={styles.nav_btn}>Logout</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
