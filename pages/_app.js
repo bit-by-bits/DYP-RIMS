e44eccaa874daa4b6bea3b55fa05116ae1854bf1
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-10-09 12:05:16 +0530

    style: indentation issues

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,11 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-export default MyApp
+  return (
+    <>
+      <script src="https://accounts.google.com/gsi/client" async defer></script>
+      <Component {...pageProps} />
+    </>
+  );
+};
+export default MyApp;
