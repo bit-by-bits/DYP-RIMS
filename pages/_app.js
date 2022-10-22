50f7107264490d326227b68c17d5b8ea7ccdccab
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-10-22 21:59:00 +0530

    fix: async google script

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,13 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import Head from "next/head";
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <Script async src="https://accounts.google.com/gsi/client" />
+      <Head>
+        <script async src="https://accounts.google.com/gsi/client" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
