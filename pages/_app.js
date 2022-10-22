50400bf2a9226ea84513f92d5880d26464e42909
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-10-22 22:27:30 +0530

    fix: sync google script

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 config.autoAddCss = false;
 import { config } from "@fortawesome/fontawesome-svg-core";
 
-import Head from "next/head";
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <script async src="https://accounts.google.com/gsi/client" />
-      </Head>
+      <script src="https://accounts.google.com/gsi/client" />
       <Component {...pageProps} />
     </>
   );
