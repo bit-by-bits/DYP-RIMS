a10b3eea5369a20ebae3e7a9bdeb87f7a3428499
Prateek Kashyap <kashyapprateek13@gmail.com>
2022-11-08 19:14:30 +0530

    fix: sync gauth link

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Script from "next/script";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <script src="https://accounts.google.com/gsi/client" />
+      <Script src="https://accounts.google.com/gsi/client" />
       <Component {...pageProps} />
     </>
   );
