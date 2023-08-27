8e99524bd90f49c8902a8e67acb1c7231642f5de
Rownak Mazumder <108619456+RownakM@users.noreply.github.com>
2023-08-27 15:59:44 +0530

    Update baseURL.js

--- src/components/baseURL.js
+++ src/components/baseURL.js
@@ -1,6 +1,6 @@
 const URLObj = {
   ai: "https://verify.journalchecker.com/pdf/",
-  base: "https://services.journalchecker.com",
+  base: "https://server.journalchecker.com",
   key: "b2b3c4a1-d8b8-47cf-8d5d-8d3bdb6b5ed7",
   dummy:
     "https://cquipsplus.ca/wp-content/themes/cera/assets/images/avatars/user-avatar-thumb.png",
